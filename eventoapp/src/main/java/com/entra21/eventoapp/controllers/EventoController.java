b472fe70159d79abd9f30fac2e94628919d2f851
Eduardo Luiz Delmonego <104005418+edueld87@users.noreply.github.com>
2022-08-30 11:53:25 -0300

    Add files via upload

---
+++ eventoapp/src/main/java/com/entra21/eventoapp/controllers/EventoController.java
@@ -0,0 +1,17 @@
+package com.entra21.eventoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class EventoController {
+	
+	@RequestMapping("/cadastrarEvento")
+	public String form() {
+		return "formEvento";
+		
+	}
+		
+
+}

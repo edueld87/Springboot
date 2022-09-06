5bbcdea911c1c90797b99dd00f6c2b6ff1e025ba
Eduardo Luiz Delmonego <104005418+edueld87@users.noreply.github.com>
2022-09-06 11:41:29 -0300

    Add files via upload

--- eventoapp/src/main/java/com/entra21/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/entra21/eventoapp/controllers/EventoController.java
@@ -1,17 +1,79 @@
 package com.entra21.eventoapp.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.entra21.eventoapp.models.Convidado;
+import com.entra21.eventoapp.models.Evento;
+import com.entra21.eventoapp.repository.ConvidadoRepository;
+import com.entra21.eventoapp.repository.EventoRepository;
 
 @Controller
 
 public class EventoController {
 	
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private EventoRepository er;
+	
+	@Autowired
+	private ConvidadoRepository cr;
+	
+	@RequestMapping(value = "/cadastrarEvento", method= RequestMethod.GET)
 	public String form() {
-		return "formEvento";
+		return "evento/formEvento";
 		
 	}
 		
+	@RequestMapping(value = "/cadastrarEvento", method= RequestMethod.POST)
+	public String form(Evento evento) {
+	er.save(evento);
+	return "redirect:/cadastrarEvento";
 
+	}
+	
+	@RequestMapping("/eventos")	
+	public ModelAndView listaEventos(){		
+		ModelAndView mv = new 				// objeto para renderiar a pagina					
+		ModelAndView("evento/listaEvento");	
+		Iterable<Evento> eventos = er.findAll();		
+		mv.addObject("leventos", eventos); // leventos atributo 								  
+										   //que está no HTML		
+		return mv;
+	}
+	
+	@RequestMapping(value ="/{codigo}", method = RequestMethod.GET)
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
+		
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
+		return mv;
+	}
+	
+	@RequestMapping(value ="/{codigo}", method = RequestMethod.POST)
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado, BindingResult result,
+			RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+	        attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+	        return "redirect:/{codigo}";
+	    }
+	        
+	    Evento evento = er.findByCodigo(codigo);
+	    convidado.setEvento(evento);
+	    cr.save(convidado);
+	    attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
+	    return "redirect:/{codigo}";
+	}
+		
 }
+
--- eventoapp/src/main/java/com/entra21/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/entra21/eventoapp/models/Evento.java
@@ -1,12 +1,20 @@
 package com.entra21.eventoapp.models;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
 
 @Entity
-public class Evento {
+public class Evento implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,7 +24,16 @@ public class Evento {
 	private String data;
 	private String horario;
 	
+	@OneToMany
+	private List<Convidado> convidados;
 	
+		
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
@@ -41,5 +58,12 @@ public class Evento {
 	public void setHorario(String horario) {
 		this.horario = horario;
 	}
+	public List<Convidado> getConvidados() {
+		return convidados;
+	}
+	public void setConvidados(List<Convidado> convidados) {
+		this.convidados = convidados;
+	}
 
+	
 }
